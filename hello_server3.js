932cdb8490149f54076028d61ba494f9bb91060d
Rafael Ferreira <rafamarques03@gmail.com>
2019-05-15 10:44:21 -0300

    lendo rotas diferentes e dando respostas diferentes usando if

---
+++ hello_server3.js
@@ -0,0 +1,19 @@
+var http= require('http');
+
+var server = http.createServer(function(request,response){
+    response.writeHead(200,{"Content-Type": "text/html"});
+
+    if(request.url == "/"){
+        response.write("<h1>Hello World</h1>");
+    }else if(request.url == "/bemvindo"){
+        response.write("<h1>Bem-Vindo :)</h1>");
+    }else{
+        response.write("<h1>Página não encontrada</h1>");
+    }
+    
+    response.end();
+});
+
+server.listen(3000, function(){
+    console.log("Servidor Rodando");
+});
